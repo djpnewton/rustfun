cab28f91ad2537f613013123d1d4f848af43422d
Daniel Newton <djpnewton@gmail.com>
2019-04-25 13:03:18 -0500

    improve rendering speed a lot (render to pixbuf, cache pixbuf)

--- fractal/src/main.rs
+++ fractal/src/main.rs
@@ -13,10 +13,11 @@ use quicksilver::{
     Future, Result,
     combinators::ok,
     load_file,
-    geom::{Rectangle, Vector},
-    graphics::{Background::Col, Color},
+    geom::{Rectangle, Vector, Shape},
+    graphics::{Background::Col, Color, PixelFormat, Image},
     input::{ButtonState, Key},
     lifecycle::{Asset, Event, Settings, State, Window, run},
+    prelude::Img,
 };
 
 mod text;
@@ -31,12 +32,13 @@ const WIN_Y: u32 = 800;
 const MAX_ITER: u8 = 80;
 const RE_START: f32 = -2.;
 const RE_END: f32 = 1.;
-const IM_START: f32 = -1.;
-const IM_END: f32 = 1.;
+const IM_START: f32 = -1.5;
+const IM_END: f32 = 1.5;
 
 enum Fractal {
-    SierpinskiTriangle { color: Color },
-    Mandelbrot,
+    None,
+    SierpinskiTriangle { img: Image },
+    Mandelbrot { img: Image },
 }
 
 struct FractalRenderer {
@@ -49,9 +51,9 @@ struct FractalRenderer {
 
 impl State for FractalRenderer {
     fn new() -> Result<FractalRenderer> {
-        let fractal = choose_fractal();
+        let fractal = change_fractal(&Fractal::None);
         let text_renderer = TextRenderer::new();
-        Ok(FractalRenderer {run: false, elapsed: 0., steps: 0, fractal: fractal, text_renderer: text_renderer})
+        Ok(FractalRenderer {run: true, elapsed: 0., steps: 0, fractal: fractal, text_renderer: text_renderer})
     }
 
     fn event(&mut self, event: &Event, _window: &mut Window) -> Result<()> {
@@ -61,12 +63,16 @@ impl State for FractalRenderer {
         if let Event::Key(Key::R, ButtonState::Pressed) = event {
             self.steps = 0;
             self.elapsed = 0.;
-            self.fractal = choose_fractal();
+            self.fractal = match &self.fractal {
+                Fractal::None => Fractal::None,
+                Fractal::SierpinskiTriangle { img } => new_sierpinski_triangle(WIN_X, WIN_Y),
+                Fractal::Mandelbrot { img } => new_mandelbrot(WIN_X, WIN_Y),
+            }
         }
         if let Event::Key(Key::C, ButtonState::Pressed) = event {
             self.steps = 0;
             self.elapsed = 0.;
-            self.fractal = choose_fractal();
+            self.fractal = change_fractal(&self.fractal);
         }
         Ok(())
     }
@@ -86,41 +92,13 @@ impl State for FractalRenderer {
     fn draw(&mut self, window: &mut Window) -> Result<()> {
         window.clear(Color::WHITE)?;
 
-        match self.fractal {
-            Fractal::SierpinskiTriangle { color } => {
-                // use bitwise arithmetic fill pixels where: x & y == 0
-                for y in 0..WIN_Y {
-                    for x in 0..WIN_X {
-                        if x & y == 0 {
-                            window.draw(&Rectangle::new((x, y), (1, 1)), Col(color));
-                        }
-                    }
-                }
+        match &self.fractal {
+            Fractal::None => (),
+            Fractal::SierpinskiTriangle { img } => {
+                window.draw(&img.area().with_center((WIN_X/2, WIN_Y/2)), Img(&img));
             }
-            Fractal::Mandelbrot => {
-                for y in 0..WIN_Y {
-                    for x in 0..WIN_X {
-                        // convert pixel coord to complex number
-                        let c = Complex::new(
-                            RE_START + (x as f32 / WIN_X as f32) * (RE_END - RE_START),
-                            IM_START + (y as f32 / WIN_Y as f32) * (IM_END - IM_START));
-                        // compute iterations
-                        let m = calc_mandelbrot(c);
-                        // color depends on the num of iterations
-                        let hue = m / MAX_ITER as f32 * 360.;
-                        let saturation = 1.;
-                        let value = if m < MAX_ITER as f32 { 1. } else { 0. };
-                        let hsv = Hsv::new(hue, saturation, value);
-                        let rgb = Srgb::from_hsv(hsv);
-                        // draw pixel
-                        let color = Color::from_rgba(
-                            (rgb.red * 255.) as u8, 
-                            (rgb.green * 255.) as u8, 
-                            (rgb.blue * 255.) as u8, 
-                            1.);
-                        window.draw(&Rectangle::new((x, y), (1, 1)), Col(color));
-                    }
-                }
+            Fractal::Mandelbrot { img } => {
+                window.draw(&img.area().with_center((WIN_X/2, WIN_Y/2)), Img(&img));
             }
         }
 
@@ -153,14 +131,85 @@ fn main() {
     run::<FractalRenderer>("FractalRenderer", Vector::new(WIN_X, WIN_Y), Settings::default());
 }
 
-fn choose_fractal() -> Fractal {
+fn change_fractal(current: &Fractal) -> Fractal {
+    match current {
+        Fractal::None => new_mandelbrot(WIN_X, WIN_Y),
+        Fractal::Mandelbrot { img } => new_sierpinski_triangle(WIN_X, WIN_Y),
+        Fractal::SierpinskiTriangle { img } => new_mandelbrot(WIN_X, WIN_Y),
+    }
+}
+
+fn new_sierpinski_triangle(max_x: u32, max_y: u32) -> Fractal {
+    // allocate pixel buffer (RGBA - 4 * u8 per pixel)
+    let mut pixels = vec![0 as u8; (4 * max_x * max_y) as usize];
+    // use bitwise arithmetic fill pixels where: x & y == 0
     let mut rng = rand::thread_rng();
-    let n: u8 = rng.gen();
-    if n % 2 == 0 {
-        let color = Color::from_rgba(rng.gen(), rng.gen(), rng.gen(), 1.);
-        Fractal::SierpinskiTriangle { color: color }
-    } else {
-        Fractal::Mandelbrot
+    let red: u8 = rng.gen();
+    let green: u8 = rng.gen();
+    let blue: u8 = rng.gen();
+    for y in 0..max_y {
+        for x in 0..max_x {
+            if x & y == 0 {
+                // draw pixel
+                let index = 4 * (x + y * max_x) as usize;
+                let bytes = [red, green, blue, 255];
+                for i in 0..bytes.len() {
+                    pixels[index + i] = bytes[i];
+                }
+            }
+        }
+    }
+    match Image::from_raw(pixels.as_slice(), max_y as u32, max_y as u32, PixelFormat::RGBA) {
+        Ok(img) => {
+            Fractal::SierpinskiTriangle { img: img }
+        }
+        Err(msg) => {
+            //debug_output(msg),
+            Fractal::None
+        }
+    }
+}
+
+fn new_mandelbrot(max_x: u32, max_y: u32) -> Fractal {
+    let mut rng = rand::thread_rng();
+    let max_iter = rng.gen_range(10, MAX_ITER);
+    // allocate pixel buffer (RGBA - 4 * u8 per pixel)
+    let mut pixels = vec![0 as u8; (4 * max_x * max_y) as usize];
+    // color the pixels
+    for y in 0..max_y {
+        for x in 0..max_x {
+            // convert pixel coord to complex number
+            let c = Complex::new(
+                RE_START + (x as f32 / max_x as f32) * (RE_END - RE_START),
+                IM_START + (y as f32 / max_y as f32) * (IM_END - IM_START));
+            // compute iterations
+            let m = calc_mandelbrot_point(c, max_iter);
+            // color depends on the num of iterations
+            let hue = m / max_iter as f32 * 360.;
+            let saturation = 1.;
+            let value = if m < max_iter as f32 { 1. } else { 0. };
+            let hsv = Hsv::new(hue, saturation, value);
+            let rgb = Srgb::from_hsv(hsv);
+            // draw pixel
+            let index = 4 * (x + y * max_x) as usize;
+            let bytes = [
+                (rgb.red * 255.) as u8,
+                (rgb.green * 255.) as u8,
+                (rgb.blue * 255.) as u8,
+                255];
+            for i in 0..bytes.len() {
+                pixels[index + i] = bytes[i];
+            }
+        }
+    }
+    match Image::from_raw(pixels.as_slice(), max_y as u32, max_y as u32, PixelFormat::RGBA) {
+        Ok(img) => {
+            Fractal::Mandelbrot { img: img }
+        }
+        Err(msg) => {
+            //debug_output(msg),
+            Fractal::None
+        }
     }
 }
 
@@ -168,16 +217,16 @@ fn complex_abs(c: Complex<f32>) -> f32 {
     (c.re * c.re + c.im * c.im).sqrt()
 }
 
-fn calc_mandelbrot(c: Complex<f32>) -> f32 {
+fn calc_mandelbrot_point(c: Complex<f32>, max_iter: u8) -> f32 {
     let mut z = Complex::new(0., 0.);
     let mut n = 0;
-    while complex_abs(z) <= 2. && n < MAX_ITER {
+    while complex_abs(z) <= 2. && n < max_iter {
         z = z*z + c;
         n += 1;
     }
     // dont just return n, 'renormalize the mandelbrot escape'
-    if n == MAX_ITER {
-        MAX_ITER as f32
+    if n == max_iter {
+        max_iter as f32
     }
     else {
         n as f32 + 1. - complex_abs(z).log2().ln()
