1658a4adafc954b23c9e03bc36a1fb344fc4d513
Daniel Newton <djpnewton@gmail.com>
2019-04-25 11:29:17 -0500

    add mandelbrot (need to speed it up)

--- fractal/src/main.rs
+++ fractal/src/main.rs
@@ -1,10 +1,14 @@
 extern crate rand;
+extern crate num;
+extern crate palette;
 extern crate quicksilver;
 #[macro_use]
 extern crate stdweb;
 
 use std::str;
 use rand::Rng;
+use num::complex::Complex;
+use palette::{Hsv, Srgb, FromColor};
 use quicksilver::{
     Future, Result,
     combinators::ok,
@@ -23,14 +27,16 @@ use crate::{
 const WIN_X: u32 = 800;
 const WIN_Y: u32 = 800;
 
+// mandelbrot consts
+const MAX_ITER: u8 = 80;
+const RE_START: f32 = -2.;
+const RE_END: f32 = 1.;
+const IM_START: f32 = -1.;
+const IM_END: f32 = 1.;
+
 enum Fractal {
     SierpinskiTriangle { color: Color },
-}
-
-struct Map {
-    x: usize,
-    y: usize,
-    state: Vec<bool>,
+    Mandelbrot,
 }
 
 struct FractalRenderer {
@@ -91,6 +97,31 @@ impl State for FractalRenderer {
                     }
                 }
             }
+            Fractal::Mandelbrot => {
+                for y in 0..WIN_Y {
+                    for x in 0..WIN_X {
+                        // convert pixel coord to complex number
+                        let c = Complex::new(
+                            RE_START + (x as f32 / WIN_X as f32) * (RE_END - RE_START),
+                            IM_START + (y as f32 / WIN_Y as f32) * (IM_END - IM_START));
+                        // compute iterations
+                        let m = calc_mandelbrot(c);
+                        // color depends on the num of iterations
+                        let hue = m / MAX_ITER as f32 * 360.;
+                        let saturation = 1.;
+                        let value = if m < MAX_ITER as f32 { 1. } else { 0. };
+                        let hsv = Hsv::new(hue, saturation, value);
+                        let rgb = Srgb::from_hsv(hsv);
+                        // draw pixel
+                        let color = Color::from_rgba(
+                            (rgb.red * 255.) as u8, 
+                            (rgb.green * 255.) as u8, 
+                            (rgb.blue * 255.) as u8, 
+                            1.);
+                        window.draw(&Rectangle::new((x, y), (1, 1)), Col(color));
+                    }
+                }
+            }
         }
 
         self.text_renderer.draw(
@@ -124,8 +155,33 @@ fn main() {
 
 fn choose_fractal() -> Fractal {
     let mut rng = rand::thread_rng();
-    let color = Color::from_rgba(rng.gen(), rng.gen(), rng.gen(), 1.);
-    Fractal::SierpinskiTriangle { color: color }
+    let n: u8 = rng.gen();
+    if n % 2 == 0 {
+        let color = Color::from_rgba(rng.gen(), rng.gen(), rng.gen(), 1.);
+        Fractal::SierpinskiTriangle { color: color }
+    } else {
+        Fractal::Mandelbrot
+    }
+}
+
+fn complex_abs(c: Complex<f32>) -> f32 {
+    (c.re * c.re + c.im * c.im).sqrt()
+}
+
+fn calc_mandelbrot(c: Complex<f32>) -> f32 {
+    let mut z = Complex::new(0., 0.);
+    let mut n = 0;
+    while complex_abs(z) <= 2. && n < MAX_ITER {
+        z = z*z + c;
+        n += 1;
+    }
+    // dont just return n, 'renormalize the mandelbrot escape'
+    if n == MAX_ITER {
+        MAX_ITER as f32
+    }
+    else {
+        n as f32 + 1. - complex_abs(z).log2().ln()
+    }
 }
 
 #[cfg(target_arch = "wasm32")]
